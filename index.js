560d1cf5074a72e30e9db0cf4171016621b6982c
Om yadav <omyaduvanshi9@gmail.com>
2024-05-16 21:19:24 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
 
-var mymap = L.map('mapid').setView([51.404, -0.09], 13);
+var mymap = L.map('mapid').setView([30.3165, 78.0322], 13);
 
 L.tileLayer(
   'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
